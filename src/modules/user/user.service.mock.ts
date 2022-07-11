97495b7568d08a5749c5c2613f6e3f17aa8489c1
Rodrigo Ramirez <xpromx@gmail.com>
2022-07-11 09:58:37 +0900

    chore: remove user.service.mock.ts

--- src/modules/user/user.service.mock.ts
+++
@@ -1,8 +0,0 @@
-import { users } from "./data";
-import { UserService } from "./user.service";
-
-export const UserServiceMock: typeof UserService = {
-  getAll: () => users,
-  getById: (id: number) => users.find((user) => user.id === id) || null,
-  create: () => users[0],
-};
